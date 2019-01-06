c48366fd00e6d2e2f31bd5cc0fb7d56b4552d04d
Damith C. Rajapakse <damith@gmail.com>
2019-01-06 15:29:44 +0800

    Site Update.

--- pullall.sh
+++ pullall.sh
@@ -8,7 +8,7 @@ git pull origin master
 cd ..
 git checkout master
 git pull upstream master
-git branch -D te3201
+git branch -D cs2103
 git fetch origin
 git checkout --track origin/te3201
 git rebase master
